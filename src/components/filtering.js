fb774799b5672f0076bddf519ef71eae6a215428
Anton Polyakov <tonussun@yandex.ru>
2026-04-30 16:49:54 +0300

    5-й коммит 6.2

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -10,25 +10,21 @@ export function initFiltering(elements, indexes) {
         elements[elementName].append(                    // в каждый элемент добавляем опции
             ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
                       .map(name => {                        // используйте name как значение и текстовое содержимое
-                        const el = document.createElement("option");
-                        el.textContent = name;
-                        el.value = name;
-                        return el;                                // @todo: создать и вернуть тег опции
+                        const option = document.createElement('option'); 
+                        option.value = name;
+                        option.textContent = name;
+                        return option;                               // @todo: создать и вернуть тег опции
                       })
         )
      }) 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-        const filterContainer = document.querySelector('[data-name="filter"]');
-        filterContainer.addEventListener("click", (e) => {
-            if (e.target.name === "clear") {
-                const parent = e.target.parentElement;
-                const clearField = parent.querySelector("input");
-                const fieldName = e.target.dataset.field;
-                clearField.value = "";
-                state[fieldName] = "";
-            }
-        });
+       if (action && action.name === "clear") {
+            const container = action.closest('.table-column');
+            const input = container.querySelector('input');
+            input.value = ''; 
+            state[action.dataset.field] = '';
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
         return data.filter(row => compare(row, state)); 
     }
--- src/main.js
+++ src/main.js
@@ -41,12 +41,10 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-    result = applyPagination(result, state, action);
     result = applySearching(result, state, action); 
-    result = applySorting(result, state, action);
     result = applyFiltering(result, state, action);
-    
-
+    result = applySorting(result, state, action);
+    result = applyPagination(result, state, action);
     sampleTable.render(result)
 }
 
@@ -58,6 +56,18 @@ const sampleTable = initTable({
 }, render);
 
 // @todo: инициализация
+
+const applySearching = initSearching('search');
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
+    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
+});
+
+const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]); 
+
 const applyPagination = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
@@ -69,18 +79,6 @@ const applyPagination = initPagination(
         return el;
     }
 ); 
-
-const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
-    sampleTable.header.elements.sortByDate,
-    sampleTable.header.elements.sortByTotal
-]); 
-
-const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
-    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
-});
-
-const applySearching = initSearching('search');
-
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
